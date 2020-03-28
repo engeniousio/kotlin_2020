bb24d37f5d31eceb03f87db789c6bbc0200ca711
Aishwarya Chakravarthy <Aishwarya@Aishwaryas-MacBook-Pro.local>
2020-03-28 13:58:11 -0700

    Created properties for StudentMaui class

---
+++ app/src/main/java/com/example/kotlinplayground/StudentMaui.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinplayground
+
+class StudentMaui(
+    private val name: String,
+    private val course: String,
+    private val degree: String,
+    private var yearAttending: Int
+) {
+    private var isPassed: Boolean? = null
+
+}
+
+
+
