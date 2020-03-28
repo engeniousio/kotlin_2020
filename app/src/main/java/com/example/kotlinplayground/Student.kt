50f0833129844c63c6d73f5cc5ed7b9170a51a3d
sam-ata <Adele020410T>
2020-03-28 15:57:04 -0700

    PROPERTIES FOR STUDENT CLASS

---
+++ app/src/main/java/com/example/kotlinplayground/Student.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinplayground
+
+ class Student {
+
+    val firstName: String,
+    val lastName: String,
+    val grade: Int,
+    val major: String,
+    val locationZipCode: Int,
+}
