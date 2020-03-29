a20f6a09f7c8b8d3c753f2ecfea77da965f75d9a
Andrei <andrei@bevager.com>
2020-03-29 00:37:06 -0700

    added student class

---
+++ app/src/main/java/com/example/kotlinplayground/Student.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinplayground
+
+class Student (
+               name: String,
+               startingYear: Int,
+               nameOfTheSchool: String,
+               avgGrage: Double) {
+
+
+
+    var drinkBeer: Boolean = true
+        private set
+
+}
