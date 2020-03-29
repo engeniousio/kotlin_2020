d955b89ca9ea20b6eb7ad8c60d5b9ca661086248
sam-ata <Adele020410T>
2020-03-29 09:48:19 -0700

    variables

--- app/src/main/java/com/example/kotlinplayground/Student.kt
+++ app/src/main/java/com/example/kotlinplayground/Student.kt
@@ -5,6 +5,20 @@ class Student (
               private val name: String,
               private var startingYear: Int,
               private val nameOfTheSchool: String,
-              private var avgGrage: Double) {
-
+              private var avgGrade: Double) {
+    ​
+    var drinkBeer: Boolean = true
+        private set
+    ​
+    fun startLearning() {
+        drinkBeer = false
+    }
+    ​
+    fun changeStartingYear(newYear: Int) {
+        startingYear = newYear
+    }
+    ​
+    fun studentData() {
+        println(" University: $nameOfTheSchool student name: $name starting year: $startingYear average grade: $avgGrade")
+    }
 }
