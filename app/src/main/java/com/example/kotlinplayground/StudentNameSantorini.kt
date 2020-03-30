a49b083924095e2d15db6d71d3e2dd44cd42d3b5
Max Egorov <yegorovmax@yahoo.com>
2020-03-30 16:07:31 -0700

    fixes for HW1

--- app/src/main/java/com/example/kotlinplayground/StudentNameSantorini.kt
+++ app/src/main/java/com/example/kotlinplayground/StudentNameSantorini.kt
@@ -6,24 +6,7 @@ class StudentNameSantorini(
             private var chemistryNumber: Int,
             private var linguisticsNumber: Int) {
 
-    fun mathExam5(): Int {
-        mathNumber = 5
-        return mathNumber
-    }
-    fun chemistryExam5(): Int {
-        chemistryNumber = 5
-        return chemistryNumber
-    }
-    fun linguisticsExam5():Int {
-        linguisticsNumber = 5
-        return linguisticsNumber
-    }
-
-
     fun averageNumber () :Double {
-        val mathNumber = mathExam5()
-        val linguisticsNumber = linguisticsExam5()
-        val chemistryNumber = chemistryExam5()
         val averageNumber: Double = ((mathNumber + linguisticsNumber + chemistryNumber)/3).toDouble()
         return averageNumber
     }
