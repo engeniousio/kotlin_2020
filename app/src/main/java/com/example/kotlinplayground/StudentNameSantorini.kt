7bf9269eb8273d3a82f61f5983ecdfceba2f3352
Max Egorov <yegorovmax@yahoo.com>
2020-03-30 15:45:15 -0700

    fixes for HW1

--- app/src/main/java/com/example/kotlinplayground/StudentNameSantorini.kt
+++ app/src/main/java/com/example/kotlinplayground/StudentNameSantorini.kt
@@ -3,58 +3,35 @@ package com.example.kotlinplayground
 import kotlin.random.Random
 
 class StudentNameSantorini(
-    private val numberOfStudentsInGroup: Int = 100,
-            private var yearMathExam: Boolean = false,
-            private var yearChemistryExam: Boolean = false,
-            private var yearLinguisticsExam: Boolean = false) {
+            private val nameOfStudent: String,
+            private var mathNumber: Int,
+            private var chemistryNumber: Int,
+            private var linguisticsNumber: Int) {
 
-    
-
-    fun exam(): Boolean {
-        // if randomInt 0 - exam failed, id randomInt is >0 exam passed
-        var randomInt = (0..15).random()
-        if (randomInt==0) {
-            yearMathExam = false
-        } else {
-            yearMathExam = true
-        }
-        
-        randomInt = (0..14).random()
-        if (randomInt==0) {
-            yearChemistryExam = false
-        } else {
-            yearChemistryExam = true
-        }
-
-        randomInt = (0..16).random()
-        if (randomInt==0) {
-            yearLinguisticsExam = false
-        } else {
-            yearLinguisticsExam = true
-        }
-        
-        if (yearLinguisticsExam && yearChemistryExam && yearMathExam) {
-            return true
-        } else {return false}
+    fun mathExam5(): Int {
+        mathNumber = 5
+        return mathNumber
+    }
+    fun chemistryExam5(): Int {
+        chemistryNumber = 5
+        return chemistryNumber
+    }
+    fun linguisticsExam5():Int {
+        linguisticsNumber = 5
+        return linguisticsNumber
     }
 
-    fun groupeTakesExam(): Int  {
 
-        var numberStudents = numberOfStudentsInGroup
-        var numberOfStudentsPassedExam = 0
-        while (numberStudents > 0) {
-            if (exam()) {
-                numberOfStudentsPassedExam += 1
-                println("exam for student # $numberStudents passed")
-            } else {
-                println("exam for student # $numberStudents failed")
-            }
-            println("number of student was examined $numberStudents")
-            numberStudents = numberStudents - 1
-        }
-
-        return numberOfStudentsPassedExam
+    fun averageNumber () :Double {
+        val mathNumber = mathExam5()
+        val linguisticsNumber = linguisticsExam5()
+        val chemistryNumber = chemistryExam5()
+        val averageNumber: Double = ((mathNumber + linguisticsNumber + chemistryNumber)/3).toDouble()
+        return averageNumber
     }
 
+    fun studentInfo() {
+        println("Student $nameOfStudent average number is ${averageNumber()}")
+    }
 }
 
